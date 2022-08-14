f69d7eec44ee0c38d502e018ec1bdc08709d0f26
Ashrit-Yarava <ashdragon95@gmail.com>
2022-08-14 00:49:45 -0400

    Finished file check.

--- src/file_check.rs
+++ src/file_check.rs
@@ -33,8 +33,70 @@
 //
 // Refer to image in the README for a visual of the individual keys.
 
+extern crate exitcode;
 use std::path::Path;
+use std::process;
 
-fn file_not_found() {}
+fn file_not_found(file: String) {
+    println!("FILE or DIRECTORY not found. Error File: {}", file);
+    process::exit(exitcode::OSFILE); // OS File error indicates a file was not found on the
+                                     // operating system.
+}
 
-pub fn ensure_notes() {}
+fn check_dir(file: &str) {
+    if !Path::new(file).is_dir() {
+        file_not_found(file.to_string());
+    }
+}
+
+fn check_file(file: &str) {
+    if !Path::new(file).is_file() {
+        file_not_found(file.to_string());
+    }
+}
+
+pub fn ensure_notes(root_dir: &str) {
+    // Check the top level directory.
+
+    check_dir(root_dir);
+
+    // The 7 primary octaves.
+    let octaves: [&str; 7] = [
+        "octave_1", "octave_2", "octave_3", "octave_4", "octave_5", "octave_6", "octave_7",
+    ];
+
+    for octave in octaves {
+        // Check the directory exists, then each of the files in it.
+        check_dir(&format!("{}/{}/", root_dir, octave));
+
+        let keys: [&str; 12] = [
+            "c.mp3",
+            "c-sharp.mp3",
+            "d.mp3",
+            "d-sharp.mp3",
+            "e.mp3",
+            "f.mp3",
+            "f-sharp.mp3",
+            "g.mp3",
+            "g-sharp.mp3",
+            "a.mp3",
+            "a-sharp.mp3",
+            "b.mp3",
+        ];
+
+        for file in keys {
+            check_file(&format!("{}/{}/{}", root_dir, octave, file));
+        }
+    }
+
+    check_dir(&format!("{}/octave_0/", root_dir));
+
+    check_file(&format!("{}/octave_0/a.mp3", root_dir));
+    check_file(&format!("{}/octave_0/a-sharp.mp3", root_dir));
+    check_file(&format!("{}/octave_0/b.mp3", root_dir));
+
+    check_dir(&format!("{}/octave_8/", root_dir));
+
+    check_file(&format!("{}/octave_8/c-sharp.mp3", root_dir));
+    check_file(&format!("{}/octave_8/c.mp3", root_dir));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod file_check;
 
 fn main() {
-    print!("{esc}c", esc = 27 as char); // Clear the terminal screen.
+    // print!("{esc}c", esc = 27 as char); // Clear the terminal screen.
     println!("Hello World!");
+    file_check::ensure_notes("keys");
 }
