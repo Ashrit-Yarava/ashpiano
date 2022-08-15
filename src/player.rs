dcd3ed44b7df8bbef54a5d7f3a45b761835c8551
Ashrit-Yarava <ashdragon95@gmail.com>
2022-08-15 15:02:29 -0400

    Add help msg.

--- src/player.rs
+++ src/player.rs
@@ -4,39 +4,62 @@ use rodio::{Decoder, OutputStream, OutputStreamHandle, Sink};
 use std::fs::File;
 use std::io::BufReader;
 
-// Declare global variables for the current octave.
-static LEFT_OCTAVE: i32 = 4;
-static RIGHT_OCTAVE: i32 = 5;
-
-fn octave_info() {
-    println!("Left Octave: {}", LEFT_OCTAVE);
-    println!("Right Octave: {}", RIGHT_OCTAVE);
+fn octave_info(left_octave: i32, right_octave: i32) {
+    println!("Left Octave: {}", left_octave);
+    println!("Right Octave: {}", right_octave);
 }
 
 fn keyboard_event_loop() {
     let stdout = Term::buffered_stdout();
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
+    let mut left_octave: i32 = 4;
+    let mut right_octave: i32 = 5;
+
     loop {
         if let Ok(character) = stdout.read_char() {
             match character {
                 // Left octave keys
-                'q' => play_sound(&stream_handle, LEFT_OCTAVE, "c"),
-                'w' => play_sound(&stream_handle, LEFT_OCTAVE, "d"),
-                'e' => play_sound(&stream_handle, LEFT_OCTAVE, "e"),
-                'r' => play_sound(&stream_handle, LEFT_OCTAVE, "f"),
-                't' => play_sound(&stream_handle, LEFT_OCTAVE, "g"),
-                'y' => play_sound(&stream_handle, LEFT_OCTAVE, "a"),
-                'u' => play_sound(&stream_handle, LEFT_OCTAVE, "b"),
+                'q' => play_sound(&stream_handle, left_octave, "c"),
+                '2' => play_sound(&stream_handle, left_octave, "c-sharp"),
+                'w' => play_sound(&stream_handle, left_octave, "d"),
+                '3' => play_sound(&stream_handle, left_octave, "d-sharp"),
+                'e' => play_sound(&stream_handle, left_octave, "e"),
+                'r' => play_sound(&stream_handle, left_octave, "f"),
+                '5' => play_sound(&stream_handle, left_octave, "f-sharp"),
+                't' => play_sound(&stream_handle, left_octave, "g"),
+                '6' => play_sound(&stream_handle, left_octave, "g-sharp"),
+                'y' => play_sound(&stream_handle, left_octave, "a"),
+                '7' => play_sound(&stream_handle, left_octave, "a-sharp"),
+                'u' => play_sound(&stream_handle, left_octave, "b"),
+
+                // Changing octaves.
+                'a' => left_octave = 1,
+                's' => left_octave = 2,
+                'd' => left_octave = 3,
+                'f' => left_octave = 4,
 
                 // Right octave keys
-                'v' => play_sound(&stream_handle, RIGHT_OCTAVE, "c"),
-                'b' => play_sound(&stream_handle, RIGHT_OCTAVE, "d"),
-                'n' => play_sound(&stream_handle, RIGHT_OCTAVE, "e"),
-                'm' => play_sound(&stream_handle, RIGHT_OCTAVE, "f"),
-                ',' => play_sound(&stream_handle, RIGHT_OCTAVE, "g"),
-                '.' => play_sound(&stream_handle, RIGHT_OCTAVE, "a"),
-                '/' => play_sound(&stream_handle, RIGHT_OCTAVE, "b"),
+                'v' => play_sound(&stream_handle, right_octave, "c"),
+                'g' => play_sound(&stream_handle, left_octave, "c-sharp"),
+                'b' => play_sound(&stream_handle, right_octave, "d"),
+                'h' => play_sound(&stream_handle, left_octave, "d-sharp"),
+                'n' => play_sound(&stream_handle, right_octave, "e"),
+                'm' => play_sound(&stream_handle, right_octave, "f"),
+                'k' => play_sound(&stream_handle, left_octave, "f-sharp"),
+                ',' => play_sound(&stream_handle, right_octave, "g"),
+                'l' => play_sound(&stream_handle, left_octave, "g-sharp"),
+                '.' => play_sound(&stream_handle, right_octave, "a"),
+                ';' => play_sound(&stream_handle, left_octave, "a-sharp"),
+                '/' => play_sound(&stream_handle, right_octave, "b"),
+
+                // Changing octaves.
+                'i' => right_octave = 5,
+                'o' => right_octave = 6,
+                'p' => right_octave = 7,
+
+                // Print which octaves are currently in use.
+                '\\' => octave_info(left_octave, right_octave),
                 _ => break,
             }
         }
@@ -52,6 +75,5 @@ fn play_sound(stream_handle: &OutputStreamHandle, octave: i32, note: &str) {
 }
 
 pub fn player() {
-    octave_info();
     keyboard_event_loop();
 }
