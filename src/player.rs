feaf6893d08e5b124dff0eb970685c46523273f6
Ashrit-Yarava <ashdragon95@gmail.com>
2022-08-14 20:41:40 -0400

    Fixed play_sound

--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,9 @@
 mod play_sound;
+use console::Term;
 
+use self::play_sound::play_sound;
+
+// Declare global variables for the current octave.
 static LEFT_OCTAVE: i32 = 4;
 static RIGHT_OCTAVE: i32 = 5;
 
@@ -10,4 +14,19 @@ fn octave_info() {
 
 pub fn player() {
     octave_info();
+
+    let stdout = Term::buffered_stdout();
+
+    loop {
+        if let Ok(character) = stdout.read_char() {
+            match character {
+                'q' => play_sound("c", LEFT_OCTAVE),
+                'w' => println!("Up"),
+                'a' => println!("Left"),
+                's' => println!("Down"),
+                'd' => println!("Right"),
+                _ => break,
+            }
+        }
+    }
 }
--- src/player/play_sound.rs
+++ src/player/play_sound.rs
@@ -1,10 +1,7 @@
-use rodio::{source::Source, Decoder, OutputStream};
-use std::fs::File;
-use std::io::BufReader;
+use play;
 
-pub fn play_sound(note: &str) {
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let file = BufReader::new(File::open(format!("keys/{}.mp3", note)).unwrap());
-    let source = Decoder::new(file).unwrap();
-    stream_handle.play_raw(source.convert_samples()).unwrap();
+pub fn play_sound(note: &str, octave: i32) {
+    let audio_file = format!("keys/octave_{}/{}.mp3", octave, note);
+
+    play::play(audio_file).unwrap();
 }
