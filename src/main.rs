e6bd29bb974a42abd7ae1e99e7ad4f9e593a7bf3
Ashrit-Yarava <ashdragon95@gmail.com>
2022-08-15 21:36:59 -0400

    Added banner for keybindings.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+mod banner;
 mod file_check;
 mod player;
 
 fn main() {
-    // print!("{esc}c", esc = 27 as char); // Clear the terminal screen.
+    print!("{esc}c", esc = 27 as char); // Clear the terminal screen
+    banner::print_banner();
     file_check::ensure_notes("keys");
     player::player();
 }
